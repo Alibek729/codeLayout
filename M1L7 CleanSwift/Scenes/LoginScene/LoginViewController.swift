7516639501487eb13f9757d80e0d790c87227d19
Alibek Kozhambekov <kozhambekovalibek@gmail.com>
2023-06-20 16:30:27 +0600

    LoginViewController is done by using PinLayout

--- M1L7 CleanSwift/Scenes/LoginScene/LoginViewController.swift
+++ M1L7 CleanSwift/Scenes/LoginScene/LoginViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SwiftUI
+import PinLayout
 
 protocol ILoginViewController: AnyObject {
 	func render(viewModel: LoginModels.ViewModel)
@@ -14,29 +15,56 @@ protocol ILoginViewController: AnyObject {
 
 class LoginViewController: UIViewController {
 
+	private lazy var textFieldLogin: UITextField = makeTextFieldLogin()
+	private lazy var textFieldPass: UITextField = makeTextFieldPass()
+	private lazy var buttonLogin: UIButton = makeButtonLogin()
+
 	var interactor: ILoginInteractor?
 	var router: ILoginRouter?
 
 	var loginText: String {
-		set { textFieldLogin.text = newValue }
-		get { textFieldLogin.text ?? ""}
-	}
+		get {
+			textFieldLogin.text ?? ""
+		}
 
-	var passText: String {
-		set { textFieldPassword.text = newValue }
-		get { textFieldPassword.text ?? ""}
-	}
-
-	@IBOutlet private weak var textFieldLogin: UITextField!
-	@IBOutlet private weak var textFieldPassword: UITextField!
-
-	@IBAction func loginPressed(_ sender: Any) {
-
-		if let email = textFieldLogin.text, let password = textFieldPassword.text {
-			let request = LoginModels.Request(login: email, password: password)
-			interactor?.login(request: request)
+		set {
+			textFieldLogin.text = newValue
 		}
 	}
+
+	var passText: String  {
+		get {
+			textFieldPass.text ?? ""
+		}
+
+		set {
+			textFieldPass.text = newValue
+		}
+	}
+
+	@objc
+	func login() {
+		let request = LoginModels.Request(login: loginText, password: passText)
+		interactor?.login(request: request)
+	}
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		setup()
+	}
+
+	private func setup() {
+		view.backgroundColor = .white
+		title = "Authorization"
+		navigationController?.navigationBar.prefersLargeTitles = true
+	}
+
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		layoutPin()
+	}
+
 }
 
 extension LoginViewController: ILoginViewController {
@@ -46,6 +74,81 @@ extension LoginViewController: ILoginViewController {
 			router?.routeToTodoList()
 		case .failure(let message):
 			router?.showError(message: message)
+		}
+	}
+}
+
+private extension LoginViewController {
+
+	func makeTextFieldLogin() -> UITextField {
+		let textField = UITextField()
+
+		textField.backgroundColor = .white
+		textField.textColor = .black
+		textField.borderStyle = .roundedRect
+		textField.translatesAutoresizingMaskIntoConstraints = false
+
+		return textField
+	}
+
+	func makeTextFieldPass() -> UITextField {
+		let textField = UITextField()
+
+		textField.backgroundColor = .white
+		textField.textColor = .black
+		textField.borderStyle = .roundedRect
+		textField.translatesAutoresizingMaskIntoConstraints = false
+
+		return textField
+	}
+
+	func makeButtonLogin() -> UIButton {
+		let button = UIButton()
+
+		button.configuration = .filled()
+		button.configuration?.cornerStyle = .medium
+		button.configuration?.baseBackgroundColor = .red
+		button.configuration?.title = "Login"
+		button.addTarget(self, action: #selector(login), for: .touchUpInside)
+		button.translatesAutoresizingMaskIntoConstraints = false
+
+		return button
+	}
+
+	func layoutPin() {
+
+		view.backgroundColor = .cyan
+		view.addSubview(textFieldLogin)
+		view.addSubview(textFieldPass)
+		view.addSubview(buttonLogin)
+
+		textFieldLogin
+			.pin
+			.vertically(view.bounds.size.height / 4)
+			.hCenter()
+			.width(Sizes.L.width)
+			.height(Sizes.L.height)
+
+		textFieldPass
+			.pin
+			.below(of: textFieldLogin, aligned: .center)
+			.margin(Sizes.Padding.normal)
+			.width(Sizes.L.width)
+			.height(Sizes.L.height)
+
+		buttonLogin
+			.pin
+			.below(of: textFieldPass, aligned: .center)
+			.margin(Sizes.Padding.double)
+			.width(Sizes.L.width)
+			.height(Sizes.L.height)
+	}
+}
+
+struct ViewControllerProvider: PreviewProvider {
+	static var previews: some View {
+		Group {
+			LoginViewController().preview()
 		}
 	}
 }
